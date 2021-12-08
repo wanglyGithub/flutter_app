ddbead64b711c55977288d4c527a2f93e935f66e
wanglyGithub <wlytctw@163.com>
2021-12-08 22:35:31 +0800

    主页框架一期自定义BottomNavigationBarView+PagerView联动效果

--- lib/pager/home_page.dart
+++ lib/pager/home_page.dart
@@ -7,16 +7,23 @@ class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return _HomePageState();
-  }
+  State<StatefulWidget> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  var currentPage = 0;
+  PageController? pageController;
 
   @override
   void initState() {
     super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController?.dispose();
   }
 
   @override
@@ -26,12 +33,30 @@ class _HomePageState extends State<HomePage> {
         title: const Text("开源中国"),
         titleTextStyle: const TextStyle(fontSize: 16),
       ),
-      body: Container(),
+      body: PageView(
+        children: [
+          Container(color: Colors.grey.shade200),
+          Container(color: Colors.redAccent),
+          Container(color: Colors.blueAccent),
+          Container(color: Colors.teal),
+        ],
+        controller: pageController,
+        physics: const BouncingScrollPhysics(),
+        onPageChanged: (page) {
+          setState(() => currentPage = page);
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: CacheKey.navigationBars,
         type: BottomNavigationBarType.fixed,
         selectedFontSize: AppFonts.fontSize,
         unselectedFontSize: AppFonts.fontSize,
+        currentIndex: currentPage,
+        onTap: (page) {
+          pageController?.animateToPage(page,
+              duration: const Duration(microseconds: 300),
+              curve: Curves.ease.flipped);
+        },
       ),
     );
   }
